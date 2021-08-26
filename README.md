7902eb48f6908643e21224479c666304d18f23e0
Ryan McKinstry <ryanmckinstry2@gmail.com>
2021-08-26 17:04:28 -0500

    implemeted logic and listeners to buttons to be functional

---
+++ README.md
@@ -0,0 +1,4 @@
+STILL NEED TO DO:
+
+Users should be able to string together several operations and get the right answer, with each pair of numbers being evaluated at a time. For example, 12 + 7 - 5 * 3 = should yield 42. An example of the behavior we’re looking for would be this student solution. Your calculator should not evaluate more than a single pair of numbers at a time. If you enter a number then an operator and another number that calculation should be displayed if your next input is an operator. The result of the calculation should be 
+used as the first number in your new calculation.
--- main.js
+++ main.js
@@ -16,7 +16,8 @@ function multiply(num1, num2){
 function divide(num1, num2){
 
     if (num2 == 0){
-    return ("nah fam")    
+        display.textContent='nahhhh lmao'
+        return (0)    
     }
     else{
         const result = num1 / num2;
@@ -30,8 +31,6 @@ function operate(num1, num2, operator){
         case '+':
             const added = add(num1,num2);
             return(added);
-            
-        
         case '-':
             const subtracted = subtract(num1,num2);
             return (subtracted);
@@ -44,3 +43,102 @@ function operate(num1, num2, operator){
     }
     
 }
+
+//prog start
+
+const numbers = document.querySelectorAll('.num')
+const operators = document.querySelectorAll('.op')
+const display = document.querySelector('.display')
+
+
+let displayValue='';
+let prevValue='';
+let userOperator='';
+
+numbers.forEach((number)=>{
+    number.addEventListener('click',()=>{
+        if(displayValue.length<12){
+            display.textContent=`${displayValue}${number.id}`;
+            displayValue= displayValue+number.id;
+        }
+    })
+})
+
+operators.forEach((operator)=>{
+    operator.addEventListener('click',()=>{
+        switch (operator.id){
+            case 'add':
+                if(displayValue==''){
+                    prevValue = 0;
+                    displayValue ='';
+                    display.textContent='0'
+                    userOperator='+'
+                    break;
+                }
+                else{
+                    prevValue=Number(displayValue);
+                    displayValue ='';
+                    display.textContent='0';
+                    userOperator='+'
+                    break;
+                }
+            case 'subtract':
+            if(displayValue==''){
+                prevValue = 0;
+                displayValue ='';
+                display.textContent='0'
+                userOperator='-'
+                break;
+            }
+            else{
+                prevValue=Number(displayValue);
+                displayValue ='';
+                display.textContent='0';
+                userOperator='-'
+                break;
+            }
+            case 'multiply':
+                if(displayValue==''){
+                    prevValue = 0;
+                    displayValue ='';
+                    display.textContent='0'
+                    userOperator='*'
+                    break;
+                }
+                else{
+                    prevValue=Number(displayValue);
+                    displayValue ='';
+                    display.textContent='0';
+                    userOperator='*'
+                    break;
+                }
+            case 'divide':
+            if(displayValue==''){
+                prevValue = 0;
+                displayValue ='';
+                display.textContent='0'
+                userOperator='/'
+                break;
+            }
+            else{
+                prevValue=Number(displayValue);
+                displayValue ='';
+                display.textContent='0';
+                userOperator='/'
+                break;
+            }
+            case 'equal':
+                if(displayValue==''){
+                    displayValue=0;
+                    console.log(operate(prevValue,displayValue,userOperator));
+                    displayValue=(operate(prevValue,Number(displayValue),userOperator));
+                    break;
+                }
+                else{
+                    display.textContent=`${(operate(prevValue,Number(displayValue),userOperator))}`
+                    displayValue=(operate(prevValue,Number(displayValue),userOperator));
+                    break;
+                }
+        }
+    })
+})
